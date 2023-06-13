31db380f6196e4ec8525c3e48edce77ddb531fa7
lseong96 <lseong96@gmail.com>
2023-06-13 17:10:21 +0900

    연습문제

---
+++ Exam/src/test1/Test03.java
@@ -0,0 +1,28 @@
+package test1;
+/**
+ * 
+ * 날짜 : 2023/06/13
+ * 이름 : 이성찬
+ * 내용 : JAVA 자료형 연습문제 
+ * 
+ */
+public class Test03 {
+	public static void main(String[] args) {
+		
+		int num = 1;
+		int result = 0;
+		
+		result = num++; 
+		System.out.println("result : " + result);
+		
+		result = ++num; 
+		System.out.println("result : " + result);
+		
+		result = num--; 
+		System.out.println("result : " + result);
+		
+		result = --num; 
+		System.out.println("result : " + result);
+		
+	}
+}
