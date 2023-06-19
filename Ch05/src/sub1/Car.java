4ce1aba3fb78b1e57f31726e7d4af18ab9e7280f
lseong96 <lseong96@gmail.com>
2023-06-19 14:05:36 +0900

    Ch05/ 파일 추가

---
+++ Ch05/src/sub1/Car.java
@@ -0,0 +1,33 @@
+package sub1;
+
+//Car 클래스 정의
+public class Car {
+	
+	// 추상화
+	// 1. 필드(속성)
+	String name;
+	String color;
+	int speed;
+	
+	public Car() {
+		
+	}
+	
+	// 2. 메서드(기능)
+	public void speedUp(int speed) {
+		this.speed += speed;
+	}
+	
+	public void speedDown(int speed) {
+		this.speed -= speed;
+	}
+	
+	public void show() {
+		System.out.println("차량명 : " + this.name);
+		System.out.println("차량색 : " + this.color);
+		System.out.println("현재속도 : " + this.speed);
+
+	}
+	
+	
+}
